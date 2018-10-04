dcb5ac0aa7bd1736c4d12ae9906843aefcf455b6
JQartas <jcuartas0611@gmail.com>
2018-10-03 19:05:36 -0500

    1
    1

--- app/src/main/java/com/app/edukt/edukt/Activities/MainActivity.java
+++ app/src/main/java/com/app/edukt/edukt/Activities/MainActivity.java
@@ -1,8 +1,9 @@
-package com.app.edukt.edukt;
+package com.app.edukt.edukt.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.app.edukt.edukt.R;
 import com.app.edukt.edukt.petitions.Petition;
 
 public class MainActivity extends AppCompatActivity {
