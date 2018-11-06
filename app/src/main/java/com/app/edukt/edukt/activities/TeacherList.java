c2bdb7916cc4be509d528ed020c4a35ac556bda2
JQartas <jcuartas0611@gmail.com>
2018-11-06 18:40:10 -0500

    MinChanges

---
+++ app/src/main/java/com/app/edukt/edukt/activities/TeacherList.java
@@ -0,0 +1,15 @@
+package com.app.edukt.edukt.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.edukt.edukt.R;
+
+public class TeacherList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_list);
+    }
+}
