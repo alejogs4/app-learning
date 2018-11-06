4f6879efeea648cd3b0594620ad553425633416e
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2018-11-06 10:19:10 -0500

    Perfil Profesores

--- app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
+++ app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
@@ -2,14 +2,20 @@ package com.app.edukt.edukt.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import com.app.edukt.edukt.R;
 
-public class activity_homepage extends AppCompatActivity {
+
+
+public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+
     }
 }
---
+++ app/src/main/java/com/app/edukt/edukt/activities/ProfessorProfile.java
@@ -0,0 +1,15 @@
+package com.app.edukt.edukt.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.edukt.edukt.R;
+
+public class ProfessorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_profile);
+    }
+}
