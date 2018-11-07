6b0b71f63e621ff012728c07c939dc447a2d01b1
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2018-11-06 19:37:16 -0500

    Homepage

--- app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
+++ app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
@@ -1,21 +1,33 @@
 package com.app.edukt.edukt.activities;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
 import retrofit2.Callback;
 import retrofit2.Response;
 
 import com.app.edukt.edukt.R;
 
+import java.util.ArrayList;
 
 
 public class Homepage extends AppCompatActivity {
+
+    private ListView lv_teacher;
+    private TextInputEditText et_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        connect();
+    }
 
-
+    private void connect(){
+        lv_teacher = findViewById(R.id.lv_teacher);
+        et_search = findViewById(R.id.et_search);
     }
 }
