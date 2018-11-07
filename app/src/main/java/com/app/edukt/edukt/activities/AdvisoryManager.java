d7074a5c0abf79fb0f76b3403273937e6c464e51
JQartas <jcuartas0611@gmail.com>
2018-11-06 19:12:25 -0500

    Get Tachers

---
+++ app/src/main/java/com/app/edukt/edukt/activities/AdvisoryManager.java
@@ -0,0 +1,13 @@
+package com.app.edukt.edukt.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdvisoryManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advisory_manager);
+    }
+}
--- app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
+++ app/src/main/java/com/app/edukt/edukt/activities/Homepage.java
@@ -2,11 +2,17 @@ package com.app.edukt.edukt.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 import com.app.edukt.edukt.R;
+import com.app.edukt.edukt.petitions.IPetitions;
+import com.app.edukt.edukt.petitions.Petition;
+import com.app.edukt.edukt.pojos.Teacher;
 
+import java.util.List;
 
 
 public class Homepage extends AppCompatActivity {
@@ -16,6 +22,22 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+    }
 
+    private void getTeacherList(String subject) {
+        Petition petitions = Petition.getInstance();
+        IPetitions iPetitions = petitions.getRetrofit().create(IPetitions.class);
+        final Call<List<Teacher>> teachersCall = iPetitions.getTeacherBySubject(subject);
+        teachersCall.enqueue(new Callback<List<Teacher>>() {
+            @Override
+            public void onResponse(Call<List<Teacher>> call, Response<List<Teacher>> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<List<Teacher>> call, Throwable t) {
+
+            }
+        });
     }
 }
